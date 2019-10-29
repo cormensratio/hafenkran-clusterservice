073660e4c2e58516f2957f5831bf8e5ce0ff3de2
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-10-29 14:15:31 +0100

    EPDS02-32 revert to using mvnw script

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/ClusterServiceApplication.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/ClusterServiceApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClusterServiceApplication
-{
+public class ClusterServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClusterServiceApplication.class, args);
     }
