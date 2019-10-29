0fd29292ebb04e942705d6bf56b14e68129b9ef3
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-10-29 10:51:57 +0100

    EPDS02-32 add main method

---
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/ClusterServiceApplication.java
@@ -0,0 +1,10 @@
+package de.unipassau.sep19.hafenkran.clusterservice.config;
+
+import org.springframework.boot.SpringApplication;
+
+public class ClusterServiceApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ClusterServiceApplication.class, args);
+    }
+}
