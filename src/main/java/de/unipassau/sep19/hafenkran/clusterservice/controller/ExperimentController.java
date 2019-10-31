b011b2f66e241e44ae7676775635d0769d6eb80e
Simon Lichtnecker <cormensratio@posteo.de>
2019-10-31 15:43:07 +0100

    EPDS02-26 Changed ImageController to ExperimentController

---
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/controller/ExperimentController.java
@@ -0,0 +1,30 @@
+package de.unipassau.sep19.hafenkran.clusterservice.controller;
+
+import de.unipassau.sep19.hafenkran.clusterservice.dto.ExperimentDTO;
+import de.unipassau.sep19.hafenkran.clusterservice.service.impl.ExperimentServiceImpl;
+import de.unipassau.sep19.hafenkran.clusterservice.service.impl.UserServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+
+@Slf4j
+@RestController
+@RequestMapping("experiments")
+public class ExperimentController {
+
+    @Autowired
+    private ExperimentServiceImpl experimentServiceImpl;
+
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    public ExperimentDTO getExperimentDTO(@NotNull @PathVariable Long id){
+        ExperimentDTO experimentDTO = experimentServiceImpl.findExperimentDTOById(id);
+        return experimentDTO;
+    }
+}
