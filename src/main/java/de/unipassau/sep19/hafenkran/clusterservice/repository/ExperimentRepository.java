9924694a89a0d8150f45269c05a94bc37afbc85c
Simon Lichtnecker <cormensratio@posteo.de>
2019-10-31 15:52:42 +0100

    EPDS02-26 Adds TestRepository + Minor changes

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/repository/ExperimentRepository.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/repository/ExperimentRepository.java
@@ -1,9 +1,9 @@
 package de.unipassau.sep19.hafenkran.clusterservice.repository;
 
-import de.unipassau.sep19.hafenkran.clusterservice.model.Image;
+import de.unipassau.sep19.hafenkran.clusterservice.model.ExperimentDetails;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImageRepository extends CrudRepository<Image, Long> {
+public interface ExperimentRepository extends CrudRepository<ExperimentDetails, Long> {
 }
---
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/repository/TestRepository.java
@@ -0,0 +1,8 @@
+package de.unipassau.sep19.hafenkran.clusterservice.repository;
+
+import de.unipassau.sep19.hafenkran.clusterservice.model.Test;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TestRepository extends CrudRepository<Test, Long> {
+
+}
