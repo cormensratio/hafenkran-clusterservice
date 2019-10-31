1216c9a15c91ca4e17a2e5de9ae4f78ca6a41937
Simon Lichtnecker <cormensratio@posteo.de>
2019-10-31 15:41:23 +0100

    EPDS02-26 Adds bean detection for repository, controller, service, model packages

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/ClusterServiceApplication.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/ClusterServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClusterServiceApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(ClusterServiceApplication.class, args);
     }
 }
