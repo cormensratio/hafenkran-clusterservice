93ec95bb8fbdb7041d6b5579fa713486f7740e45
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-11-08 11:10:56 +0100

    EPDS02-77 disable cors

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/SecurityConfig.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/experiments**").permitAll();
         http.csrf().disable();
+        http.cors().disable();
     }
 
     @Bean
