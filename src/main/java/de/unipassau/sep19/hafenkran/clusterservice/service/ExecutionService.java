dd5c94032baf4823be2f6c67631bb732fff227d0
fritschk <fritschk@fim.uni-passau.de>
2019-11-12 12:28:21 +0100

    EPDS02-87 add findExecution services

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/service/ExecutionService.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/service/ExecutionService.java
@@ -1,13 +1,38 @@
 package de.unipassau.sep19.hafenkran.clusterservice.service;
 
+import de.unipassau.sep19.hafenkran.clusterservice.dto.ExecutionDTO;
+import de.unipassau.sep19.hafenkran.clusterservice.dto.ExecutionDTOList;
 import de.unipassau.sep19.hafenkran.clusterservice.model.ExecutionDetails;
 import lombok.NonNull;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ExecutionService {
 
-    public ExecutionDetails createExecution(@NonNull ExecutionDetails executionDetails);
+    /**
+     * Persists a given {@link ExecutionDetails} in the experiment database.
+     *
+     * @param executionDetails The {@link ExecutionDetails}, which are going to be persisted in the database.
+     * @return The persisted {@link ExecutionDetails}.
+     */
+    ExecutionDetails createExecution(@NonNull ExecutionDetails executionDetails);
 
-    public ExecutionDetails findExecutionById(@NonNull UUID id);
+    /**
+     * @param id The UUID of the requested {@link ExecutionDetails}.
+     * @return The requested {@link ExecutionDetails} by its {@code id}.
+     */
+    ExecutionDetails findExecutionById(@NonNull UUID id);
+
+    /**
+     * @param experimentId The UUID of the requested {@link ExecutionDTO}.
+     * @return The requested {@link ExecutionDTO} by its {@code id}.
+     */
+    ExecutionDTO findExecutionDTOByExperimentId(@NonNull UUID experimentId);
+
+    /**
+     * @param experimentId The {@code userId} of the owner's {@link ExecutionDTOList}
+     * @return The list of {@link ExecutionDTO}s of the requested {@code userId}.
+     */
+    List<ExecutionDTO> findExecutionsDTOListOfExperimentId(@NonNull UUID experimentId);
 }
