6eab2dca5ccfa9662917df68beb4ce564fc6bfb8
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-11-12 16:26:02 +0100

    EPDS02-98 add JWT validation

---
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/JwtRequestFilter.java
@@ -0,0 +1,67 @@
+package de.unipassau.sep19.hafenkran.clusterservice.config;
+
+import de.unipassau.sep19.hafenkran.clusterservice.dto.UserDTO;
+import de.unipassau.sep19.hafenkran.clusterservice.util.JwtTokenUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * A {@link OncePerRequestFilter} for verifying the JWT sent in the header of a client request.
+ */
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @NonNull
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+        UUID userId = null;
+        String jwtToken = null;
+
+        // Retrieve the userId from the JWT if it exists
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
+            try {
+                userId = jwtTokenUtil.getUserIdFromToken(jwtToken);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unable to get JWT Token");
+            } catch (ExpiredJwtException e) {
+                System.out.println("JWT Token has expired");
+            }
+        }
+
+        // Set the UserDto as authentication object for the current session if the user is authenticated with a token
+        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDTO userDto = jwtTokenUtil.getUserDTOFromToken(jwtToken);
+
+            if (jwtTokenUtil.validateToken(jwtToken, userDto)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDto, null, null);
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+}
