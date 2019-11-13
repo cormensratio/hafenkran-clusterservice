1eaab9cc318d6dc41bca7df2ec0276dee97d4f51
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-11-13 13:28:52 +0100

    EPDS02-98 cleanup

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/util/SecurityContextUtil.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/util/SecurityContextUtil.java
@@ -1,19 +1,26 @@
 package de.unipassau.sep19.hafenkran.clusterservice.util;
 
+import de.unipassau.sep19.hafenkran.clusterservice.config.JwtAuthentication;
 import de.unipassau.sep19.hafenkran.clusterservice.dto.UserDTO;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 
+/**
+ * A utility method for extracting information about the current user stored in the {@link org.springframework.security.core.context.SecurityContext}.
+ */
 public class SecurityContextUtil {
 
-    public static UserDTO getCurrentUserDTO(){
+    /**
+     * Retrieves the {@link UserDTO} for the current user.
+     *
+     * @return {@link UserDTO} with information about the current user.
+     */
+    public static UserDTO getCurrentUserDTO() {
         final UserDTO currentUser;
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth instanceof UsernamePasswordAuthenticationToken) {
-            UsernamePasswordAuthenticationToken authToken = (UsernamePasswordAuthenticationToken) auth;
-            currentUser = (UserDTO) authToken.getPrincipal();
+        if (auth instanceof JwtAuthentication) {
+            JwtAuthentication authToken = (JwtAuthentication) auth;
+            currentUser = (UserDTO) authToken.getDetails();
         } else {
             throw new RuntimeException("Invalid user session");
         }
