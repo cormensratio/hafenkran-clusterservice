6c441c526d6d6b18196ba71de02d71bdaaa445e0
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-11-16 15:10:54 +0100

    EPDS02-73 handle field validation errors

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/ConfigEntrypoint.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/config/ConfigEntrypoint.java
@@ -5,6 +5,15 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @ComponentScan(basePackages = {
@@ -20,5 +29,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 @EnableAutoConfiguration
 public class ConfigEntrypoint {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public void handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid parameters.", ex);
+    }
 
 }
