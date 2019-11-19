6032da901c5f654492f8873105e1d5bd0f342933
Martin Loos <martinjloos@mail.de>
2019-11-19 15:17:52 +0100

    EPDS02-95 added interface KubernetesClient

---
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/kubernetesclient/KubernetesClient.java
@@ -0,0 +1,18 @@
+package de.unipassau.sep19.hafenkran.clusterservice.kubernetesclient;
+
+import java.io.IOException;
+
+/**
+ * Interface for possible KubernetesClient Implementations.
+ */
+public interface KubernetesClient {
+
+    /**
+     * Initialise the KubernetesClient depending on the
+     * property condition mockKubernetesClient.
+     *
+     * @throws IOException if the config file can't be found
+     */
+    void initKubeClient() throws IOException;
+
+}
