f7b0ea47f268df0223bc85db2ff5fd5e86c55554
Martin Loos <martinjloos@mail.de>
2019-11-28 22:54:27 +0100

    EPDS02-138 updated KubernetesClient interface documentation

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/kubernetesclient/KubernetesClient.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/kubernetesclient/KubernetesClient.java
@@ -2,25 +2,29 @@ package de.unipassau.sep19.hafenkran.clusterservice.kubernetesclient;
 
 import io.kubernetes.client.ApiException;
 
-import java.util.UUID;
-
 /**
  * Interface providing methods for interacting with a KubernetesClient.
  */
 public interface KubernetesClient {
 
     /**
-     * Creates the Pod in Kubernetes.
+     * Creates Kubernetes Pod.
      *
-     * @return the name of the pod.
-     * @throws ApiException if the pod can't be created
+     * @param userName  name of the user who creates the pod
+     * @param experimentName name of the experiment which should be deployed
+     * @param executionName name of the execution which should be deployed as a pod in kubernetes
+     * @return the name of the pod in kubernetes
+     * @throws ApiException if the communication with the api results in an error
      */
     String createPod(String userName, String experimentName, String executionName) throws ApiException;
 
     /**
-     * Deletes the Pod in Kubernetes.
+     * Deletes Kubernetes Pod.
      *
-     * @throws ApiException if the pod can't be deleted
+     * @param userName name of the user who is owner of the pod
+     * @param experimentName  name of the experiment where the execution is stored
+     * @param podName name of the pod which should be deleted from kubernetes
+     * @throws ApiException if the communication with the api results in an error
      */
     void deletePod(String userName, String experimentName, String podName) throws ApiException;
 
