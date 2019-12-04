3207ec016470832b784cf7c67a02a3b04b6e33e9
Martin Loos <martinjloos@mail.de>
2019-12-04 03:23:30 +0100

    EPDS02-138 merged develop into branch

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/kubernetesclient/impl/KubernetesClientMockImpl.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/kubernetesclient/impl/KubernetesClientMockImpl.java
@@ -1,11 +1,14 @@
 package de.unipassau.sep19.hafenkran.clusterservice.kubernetesclient.impl;
 
 import de.unipassau.sep19.hafenkran.clusterservice.kubernetesclient.KubernetesClient;
+import de.unipassau.sep19.hafenkran.clusterservice.model.ExecutionDetails;
 import io.kubernetes.client.ApiException;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.util.UUID;
+
 /**
  * Implementation of Kubernetes Mock Client for test purposes,
  * where Kubernetes isn't needed.
@@ -16,20 +19,18 @@ public class KubernetesClientMockImpl implements KubernetesClient {
 
     /**
      * Constructor of KubernetesClientMockImpl.
-     *
+     * <p>
      * Prints out info that mockKubernetesClient is used.
-     *
-     * @throws IOException Exception never thrown
      */
-    public KubernetesClientMockImpl() throws IOException {
-        log.info("Kubernetes Mock Client. Set mockKubernetesClient to false in application-dev.yml" +
+    public KubernetesClientMockImpl() {
+        log.info("Using KubernetesMockClient: Set mockKubernetesClient to false in application-dev.yml" +
                 " if you want to use Kubernetes.");
     }
 
     /**
      * {@inheritDoc}
      */
-    public String createPod(String userName, String experimentName, String executionName) throws ApiException {
+    public String createPod(String userName, String experimentName, String executionName, UUID experimentId) throws ApiException {
         log.info("KubernetesClientMockImpl can not create a Pod.");
         return "No Pod created. KubernetesClientMockImpl.";
     }
@@ -40,4 +41,15 @@ public class KubernetesClientMockImpl implements KubernetesClient {
     public void deletePod(String userName, String experimentName, String podName) throws ApiException {
         log.info("KubernetesClientMockImpl can not delete a Pod.");
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String retrieveLogs(@NonNull String userName, @NonNull ExecutionDetails executionDetails, int lines, Integer sinceSeconds, boolean withTimestamp) {
+        log.info(String.format(
+                "KubernetesClientMockImpl: Retrieving first %s lines printed to the log since %s for pod %s with id %s from user %s",
+                lines, sinceSeconds, executionDetails.getPodName(), executionDetails.getId(), userName));
+        return String.format("this is a test log for %s \n 1 \n 2", executionDetails.getPodName());
+    }
+
 }
