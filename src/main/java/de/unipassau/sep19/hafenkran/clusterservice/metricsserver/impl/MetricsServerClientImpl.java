3c59fb00cd09ceadb1ea152d1fe6f2982a92336a
Martin Loos <martinjloos@mail.de>
2020-01-15 00:04:51 +0100

    EPDS02-50 EPDS02-210 updated MetricServerClientImpl

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/metricsserver/impl/MetricsServerClientImpl.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/metricsserver/impl/MetricsServerClientImpl.java
@@ -1,11 +1,13 @@
 package de.unipassau.sep19.hafenkran.clusterservice.metricsserver.impl;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import de.unipassau.sep19.hafenkran.clusterservice.config.SpringContext;
 import de.unipassau.sep19.hafenkran.clusterservice.dto.MetricsDTO;
 import de.unipassau.sep19.hafenkran.clusterservice.metricsserver.MetricsServerClient;
 
-import lombok.RequiredArgsConstructor;
+import de.unipassau.sep19.hafenkran.clusterservice.service.ExecutionService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
@@ -15,29 +17,45 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 import java.util.UUID;
+import java.util.Date;
+
 
 /**
  * Implementation of MetricsServerClient to communicate with the MetricsServer via an api call.
  */
 @Slf4j
 @Component
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class MetricsServerClientImpl implements MetricsServerClient {
 
     @Value("${clusterProxy.path}")
     private String clusterProxyPath;
 
+    private ExecutionService executionService;
+
+    private List<String> excludedNamespaceList;
+
+    public MetricsServerClientImpl() {
+        this.executionService = SpringContext.getBean(ExecutionService.class);
+        buildNamespaceExclusionList();
+    }
+
     /**
      * {@inheritDoc}
      */
     @Override
-    public Map<String, MetricsDTO> retrieveMetrics() {
+    public ArrayList<MetricsDTO> retrieveMetrics() {
         String path = "/apis/metrics.k8s.io/v1beta1/pods";
         String jsonGetResponse = get(path, String.class);
         return retrieveMetricsFromGetRequest(jsonGetResponse);
+    }
+
+    private void buildNamespaceExclusionList() {
+        excludedNamespaceList = Arrays.asList("kube-node-lease", "kube-public", "kube-system", "kubernetes-dashboard");
     }
 
     private <T> T get(String path, Class<T> responseType) {
@@ -64,29 +82,44 @@ public class MetricsServerClientImpl implements MetricsServerClient {
      * @param jsonGetResponse The response of the metrics api call represented as string.
      * @return Returns a map with the wanted metrics of pods which arent internal.
      */
-    private Map<String, MetricsDTO> retrieveMetricsFromGetRequest(String jsonGetResponse) {
-        Map<String, MetricsDTO> mapMetrics = new HashMap<>();
+    private ArrayList<MetricsDTO> retrieveMetricsFromGetRequest(String jsonGetResponse) {
+        ArrayList<MetricsDTO> allPodMetrics = new ArrayList<>();
         try {
             JSONObject jsonGetResponseObject = new JSONObject(jsonGetResponse);
             JSONArray jsonMetricsItemsArray = jsonGetResponseObject.getJSONArray("items");
             if (jsonMetricsItemsArray != null) {
                 for (int i = 0; i < jsonMetricsItemsArray.length(); i++) {
-                    JSONObject jsonItemMetadata = jsonMetricsItemsArray.getJSONObject(i).getJSONObject("metadata");
-                    String podName = jsonItemMetadata.get("name").toString();
-                    String namespace = jsonItemMetadata.get("namespace").toString();
-                    if (!namespace.equals("kube-system") && !namespace.equals("kubernetes-dashboard")) {
-                        UUID experimentId = UUID.fromString(namespace);
-                        JSONObject jsonItemContainerUsage = jsonMetricsItemsArray.getJSONObject(i).getJSONArray("containers").getJSONObject(0).getJSONObject("usage");
-                        String cpu = jsonItemContainerUsage.get("cpu").toString();
-                        String memory = jsonItemContainerUsage.get("memory").toString();
-                        MetricsDTO metricsDTO = new MetricsDTO(null, experimentId, cpu, memory);
-                        mapMetrics.put(podName, metricsDTO);
+                    String jsonDataSourceString = jsonMetricsItemsArray.getJSONObject(i).toString();
+                    MetricsDTO metricsDTO = buildPodMetricsDTOFromJsonString(jsonDataSourceString);
+                    if (metricsDTO != null) {
+                        allPodMetrics.add(metricsDTO);
                     }
                 }
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return mapMetrics;
+        return allPodMetrics;
+    }
+
+    private MetricsDTO buildPodMetricsDTOFromJsonString(String jsonDataSourceString) {
+        String jsonPathPodNamePath = "$['metadata']['name']";
+        String jsonPathNamespacePath = "$['metadata']['namespace']";
+        String jsonPathCpuUsagePath = "$['containers'][0]['usage']['cpu']";
+        String jsonPathMemoryUsagePath = "$['containers'][0]['usage']['memory']";
+        DocumentContext jsonContext = JsonPath.parse(jsonDataSourceString);
+        String podName = jsonContext.read(jsonPathPodNamePath);
+        String namespace = jsonContext.read(jsonPathNamespacePath);
+        if (!excludedNamespaceList.contains(namespace)) {
+            UUID experimentId = UUID.fromString(namespace);
+            UUID executionId = executionService.getExecutionOfPod(podName, experimentId).getId();
+            String cpu = jsonContext.read(jsonPathCpuUsagePath);
+            String memory = jsonContext.read(jsonPathMemoryUsagePath);
+            Date date = new Date();
+            long time = date.getTime();
+            Timestamp timestamp = new Timestamp(time);
+            return new MetricsDTO(executionId, experimentId, cpu, memory, timestamp);
+        }
+        return null;
     }
 }
