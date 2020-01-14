ed4815e125ae6f63135213e0942f217dbb16c0bc
Martin Loos <martinjloos@mail.de>
2020-01-14 15:14:56 +0100

    EPDS02-210 added MetricsServerServiceImpl

---
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/service/impl/MetricsServerServiceImpl.java
@@ -0,0 +1,24 @@
+package de.unipassau.sep19.hafenkran.clusterservice.service.impl;
+
+import de.unipassau.sep19.hafenkran.clusterservice.dto.MetricsDTO;
+import de.unipassau.sep19.hafenkran.clusterservice.service.MetricsServerService;
+import de.unipassau.sep19.hafenkran.clusterservice.metricsserver.MetricsServerClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class MetricsServerServiceImpl implements MetricsServerService {
+
+    private final MetricsServerClient metricsServerClient;
+
+    @Override
+    public Map<String, MetricsDTO> retrieveMetrics() {
+        return metricsServerClient.retrieveMetrics();
+    }
+}
