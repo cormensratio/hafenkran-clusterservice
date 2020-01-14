0e6c8caf926a2a1e8a5a4e96f91fecb4d693ce28
Martin Loos <martinjloos@mail.de>
2020-01-14 14:23:27 +0100

    EPDS02-209 added MetricsDTO

---
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/dto/MetricsDTO.java
@@ -0,0 +1,41 @@
+package de.unipassau.sep19.hafenkran.clusterservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.UUID;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MetricsDTO {
+
+    @JsonProperty
+    private UUID executionId;
+
+    @NonNull
+    @JsonProperty("experimentId")
+    private final UUID experimentId;
+
+    @NonNull
+    @JsonProperty("cpu")
+    private final String cpu;
+
+    @NonNull
+    @JsonProperty("memory")
+    private final String memory;
+
+    @JsonCreator
+    public MetricsDTO(@NonNull UUID experimentId, @NonNull String cpu, @NonNull String memory) {
+        this.experimentId = experimentId;
+        this.cpu = cpu;
+        this.memory = memory;
+    }
+
+    public void setExecutionId(@NonNull UUID executionId) {
+        this.executionId = executionId;
+    }
+}
