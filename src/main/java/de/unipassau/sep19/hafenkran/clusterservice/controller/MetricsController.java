39cb83ce1f3de6ad9b2a1b8402b7cfdc0ce68d29
Martin Loos <martinjloos@mail.de>
2020-01-15 00:00:52 +0100

    EPDS02-50 EPDS02-210 updated MetricsController

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/controller/MetricsController.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/controller/MetricsController.java
@@ -1,19 +1,22 @@
 package de.unipassau.sep19.hafenkran.clusterservice.controller;
 
 import de.unipassau.sep19.hafenkran.clusterservice.dto.MetricsDTO;
-import de.unipassau.sep19.hafenkran.clusterservice.model.ExecutionDetails;
-import de.unipassau.sep19.hafenkran.clusterservice.service.ExecutionService;
 import de.unipassau.sep19.hafenkran.clusterservice.service.MetricsServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.UUID;
 
+/**
+ * The REST-Controller with the GET-request endpoint for retrieving all metrics.
+ */
 @Slf4j
 @RestController
 @RequestMapping("/metrics")
@@ -22,24 +25,15 @@ public class MetricsController {
 
     private final MetricsServerService metricsServerService;
 
-    private final ExecutionService executionService;
-
-    @GetMapping(value = "/retrieve")
+    /**
+     * GET-Endpoint for receiving a ArrayList with all {@link MetricsDTO}s.
+     *
+     * @return ArrayList with all {@link MetricsDTO}s.
+     */
+    @GetMapping(value = "/all")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     public ArrayList<MetricsDTO> retrieveMetrics() {
-        Map<String, MetricsDTO> map = metricsServerService.retrieveMetrics();
-        ArrayList<MetricsDTO> metricsDTOArrayList = new ArrayList<>();
-        for (Map.Entry<String, MetricsDTO> entry : map.entrySet()) {
-            String podName = entry.getKey();
-            MetricsDTO metricsDTO = entry.getValue();
-            if (metricsDTO.getExecutionId() == null) {
-                UUID experimentId = metricsDTO.getExperimentId();
-                ExecutionDetails executionDetails = executionService.getExecutionOfPod(podName, experimentId);
-                metricsDTO.setExecutionId(executionDetails.getId());
-            }
-            metricsDTOArrayList.add(metricsDTO);
-        }
-        return metricsDTOArrayList;
+        return metricsServerService.retrieveMetrics();
     }
 }
