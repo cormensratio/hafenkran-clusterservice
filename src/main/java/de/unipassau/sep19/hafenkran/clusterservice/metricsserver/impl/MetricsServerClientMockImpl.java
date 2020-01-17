e994ff6f5a10befbda1a1cc4b1451d631f40f4d9
Martin Loos <martinjloos@mail.de>
2020-01-17 01:05:42 +0100

    EPDS02-50 EPDS02-210 changed MetricsServerClientMockImpl

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/metricsserver/impl/MetricsServerClientMockImpl.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/metricsserver/impl/MetricsServerClientMockImpl.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.unipassau.sep19.hafenkran.clusterservice.dto.MetricDTO;
 import de.unipassau.sep19.hafenkran.clusterservice.metricsserver.MetricsServerClient;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -17,6 +20,8 @@ import java.util.UUID;
 @Slf4j
 @Component
 public class MetricsServerClientMockImpl implements MetricsServerClient {
+
+    String regex = "[^0-9]";
 
     /**
      * Constructor of MetricsServerClientMockImpl.
@@ -33,14 +38,55 @@ public class MetricsServerClientMockImpl implements MetricsServerClient {
         ArrayList<MetricDTO> mockPodMetricsList = new ArrayList<>();
         ObjectMapper objectMapper = new ObjectMapper();
         MetricDTO metricDTO = null;
+        MetricDTO metricDTOTwo = null;
+        MetricDTO metricDTOThree = null;
         String jsonDataSourceString = "{\"metadata\":{\"name\":\"hafenkran-1\",\"namespace\":\"cce4a685-391c-474b-a2ca-7e314edde99c\",\"creationTimestamp\":\"2020-01-16T14:51:14Z\",\"selfLink\":\"/apis/metrics.k8s.io/v1beta1/namespaces/cce4a685-391c-474b-a2ca-7e314edde99c/pods/hafenkran-1\"},\"containers\":[{\"usage\":{\"memory\":\"16496Ki\",\"cpu\":\"0\"},\"name\":\"hafenkran-1\"}],\"window\":\"1m0s\",\"timestamp\":\"2020-01-16T14:51:00Z\"}";
         try {
             metricDTO = objectMapper.readValue(jsonDataSourceString, MetricDTO.class);
             metricDTO.setExecutionId(UUID.fromString("00000000-0000-0000-0000-000000000001"));
+            metricDTO.setOwnerId(UUID.fromString("00000000-0000-0000-0000-000000000001"));
+            regexCpuUsage(metricDTO);
+            regexMemoryUsage(metricDTO);
+
+            metricDTOTwo = objectMapper.readValue(jsonDataSourceString, MetricDTO.class);
+            metricDTOTwo.setExecutionId(UUID.fromString("00000000-0000-0000-0000-000000000002"));
+            metricDTOTwo.setOwnerId(UUID.fromString("00000000-0000-0000-0000-000000000002"));
+            regexCpuUsage(metricDTOTwo);
+            regexMemoryUsage(metricDTOTwo);
+
+            metricDTOThree = objectMapper.readValue(jsonDataSourceString, MetricDTO.class);
+            metricDTOThree.setExecutionId(UUID.fromString("00000000-0000-0000-0000-000000000003"));
+            metricDTOThree.setOwnerId(UUID.fromString("00000000-0000-0000-0000-000000000003"));
+            regexCpuUsage(metricDTOThree);
+            regexMemoryUsage(metricDTOThree);
+
+            Date date = new Date();
+            long time = date.getTime();
+            Timestamp timestamp = new Timestamp(time);
+
+            metricDTO.setTimestamp(timestamp);
+            metricDTOTwo.setTimestamp(timestamp);
+            metricDTOThree.setTimestamp(timestamp);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
         mockPodMetricsList.add(metricDTO);
+        mockPodMetricsList.add(metricDTOTwo);
+        mockPodMetricsList.add(metricDTOThree);
         return mockPodMetricsList;
+    }
+
+    private void regexCpuUsage(@NonNull MetricDTO metricDTO) {
+        for (int i = 0; i < metricDTO.getContainers().size(); i++) {
+            String cpuUsage = metricDTO.getContainers().get(i).getUsage().getCpu();
+            metricDTO.getContainers().get(i).getUsage().setCpu(cpuUsage.replaceAll(regex, ""));
+        }
+    }
+
+    private void regexMemoryUsage(@NonNull MetricDTO metricDTO) {
+        for (int i = 0; i < metricDTO.getContainers().size(); i++) {
+            String memoryUsage = metricDTO.getContainers().get(i).getUsage().getMemory();
+            metricDTO.getContainers().get(i).getUsage().setMemory(memoryUsage.replaceAll(regex, ""));
+        }
     }
 }
