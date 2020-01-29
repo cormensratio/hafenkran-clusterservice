d5d3f994ed33367b66683f46277acbf02a4589f6
Martin Loos <martinjloos@mail.de>
2020-01-29 15:37:55 +0100

    EPDS02-210 EPDS02-50 updated KubernetesClientImpl

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/kubernetesclient/impl/KubernetesClientImpl.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/kubernetesclient/impl/KubernetesClientImpl.java
@@ -21,6 +21,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.InternalServerErrorException;
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -64,23 +65,29 @@ public class KubernetesClientImpl implements KubernetesClient {
     @Value("${kubernetes.debugging}")
     private boolean debugMode;
 
+    @Value("${kubernetes.config.path}")
+    private String kubernetesConfigLocation;
+
     @Value("${kubernetes.config.load-default}")
     private boolean loadDefaultConfig;
-
-    @Value("${kubernetes.config.location}")
-    private String kubernetesConfigLocation;
 
     /**
      * Constructor of KubernetesClientImpl.
      * <p>
      * Auto detects kubernetes config files to connect to the client and sets
      * up the api to access the cluster.
-     *
-     * @throws IOException if the config file can't be found
      */
-    public KubernetesClientImpl() throws IOException {
-        log.info("Kubernetes Client ready!");
+    public KubernetesClientImpl() {
 
+    }
+
+    /**
+     * Due to the manual initialization of the KubernetesClient in the ConfigEntrypoint the @Value marked fields are
+     * only injected after the construction, which means that all config related fields are null during the construction
+     * of the class.
+     */
+    @PostConstruct
+    private void postConstruct() throws IOException {
         // load kubernetes config file
         final ApiClient client = loadDefaultConfig
                 ? Config.defaultClient()
@@ -95,8 +102,11 @@ public class KubernetesClientImpl implements KubernetesClient {
 
         // the CoreV1Api loads default api-client from global configuration
         api = new CoreV1Api(client);
+        log.info("Kubernetes Client ready!");
+
         factory = new SharedInformerFactory();
-        createAndStartPodInformer();
+        //createAndStartPodInformer();
+        log.info("Kubernetes Pod informer ready!");
     }
 
     /**
@@ -198,7 +208,7 @@ public class KubernetesClientImpl implements KubernetesClient {
                         false,
                         false);
 
-        String output = "";
+        final String output;
         try (InputStream is = new Base64InputStream(new BufferedInputStream(proc.getInputStream()))) {
             output = IOUtils.toString(is, StandardCharsets.UTF_8);
         } catch (IOException e) {
@@ -392,4 +402,5 @@ public class KubernetesClientImpl implements KubernetesClient {
         podInformer.addEventHandler(new PodEventHandler());
         factory.startAllRegisteredInformers();
     }
+
 }
