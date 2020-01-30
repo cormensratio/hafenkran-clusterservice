11b041b2e373f14ebc3c64efd76f6980db384e4a
Martin Loos <martinjloos@mail.de>
2020-01-30 19:20:26 +0100

    EPDS02-257 implemented interface method

--- src/main/java/de/unipassau/sep19/hafenkran/clusterservice/service/MetricsService.java
+++ src/main/java/de/unipassau/sep19/hafenkran/clusterservice/service/MetricsService.java
@@ -1,6 +1,7 @@
 package de.unipassau.sep19.hafenkran.clusterservice.service;
 
 import de.unipassau.sep19.hafenkran.clusterservice.dto.MetricDTO;
+import de.unipassau.sep19.hafenkran.clusterservice.dto.NodeMetricsDTO;
 
 import java.util.ArrayList;
 
@@ -8,5 +9,18 @@ import java.util.ArrayList;
  * The MetricsServerService for retrieving all podMetrics.
  */
 public interface MetricsService {
+
+    /**
+     * Retrieves the metrics of all not cluster internal pods.
+     *
+     * @return Returns Arraylist of all metrics of not cluster internal pods.
+     */
     ArrayList<MetricDTO> retrieveMetrics();
+
+    /**
+     * Retrieves the node metrics of all cluster nodes.
+     *
+     * @return Returns Arraylist of all node metrics of the cluster.
+     */
+    ArrayList<NodeMetricsDTO> retrieveNodeMetrics();
 }
